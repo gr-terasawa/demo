3cd88520fc5fb0e83f6991d4fcb930996024459d
gr-terasawa <takeharu.terasawa@gmo-research.jp>
2015-02-26 20:07:21 +0900

    遷移先のview名をアサートするように変更

--- src/test/java/com/git/demo/HomeControllerTest.java
+++ src/test/java/com/git/demo/HomeControllerTest.java
@@ -40,7 +40,8 @@ public class HomeControllerTest {
 	@Test
 	public void ルートにGETするとhome画面に遷移する() throws Exception {
 		this.mockMvc.perform(get("/"))
-		.andExpect(status().isOk());
+		.andExpect(status().isOk())
+		.andExpect(view().name("home"));
 	}
 
 }
