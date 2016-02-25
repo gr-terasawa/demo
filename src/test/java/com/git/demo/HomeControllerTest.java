1d05bd9b8e90b0fdb9d000660961b41a268b6735
gr-terasawa <takeharu.terasawa@gmo-research.jp>
2016-02-25 19:40:02 +0900

    テストを作成しました

---
+++ src/test/java/com/git/demo/HomeControllerTest.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package com.git.demo;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+import static org.hamcrest.Matchers.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+
+/**
+ * @author usr160056
+ *
+ */
+@WebAppConfiguration
+@ContextConfiguration(locations = {
+		"file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml",
+		"classpath:test-context.xml" })
+@RunWith(SpringJUnit4ClassRunner.class)
+public class HomeControllerTest {
+
+	private MockMvc mockMvc;
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		this.mockMvc = standaloneSetup(new HomeController()).build();
+	}
+
+	@Test
+	public void ルートにGETするとhome画面に遷移する() throws Exception {
+		this.mockMvc.perform(get("/"))
+		.andExpect(status().isOk());
+	}
+
+}
